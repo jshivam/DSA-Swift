6d53b0c0a0f1d96e8a7688b7615076bb4bfa1231
Shivam Jaiswal <shivam.jaiswal@lalamove.com>
2020-04-22 20:15:02 +0530

    update: [Math][Power]

--- DSA-Swift/SocurceCode/Math/Power.swift
+++ DSA-Swift/SocurceCode/Math/Power.swift
@@ -9,22 +9,20 @@
 import Foundation
 
 class Pwoer: Executable {
-
     func excecute() {
-        print(myPow(0.00001, 58110))
+        print(myPow(0.00001, 2147483647))
     }
     
     func myPow(_ number: Double, _ power: Int ) -> Double {
-        
         if power == 0 {
             return 1
-        }
-                
-        if number > 0 {
-            return number * myPow(number, power - 1)
-        }
-        else {
-            return 1 / (number * myPow(number, -power - 1))
+        } else if power < 0 {
+            return 1/myPow(number,-power)
+        } else if power % 2 == 0 {
+            let half = myPow(number, power/2)
+            return half * half
+        } else {
+            return number * myPow(number, power-1)
         }
     }
 }
--- DSA-Swift/ViewController.swift
+++ DSA-Swift/ViewController.swift
@@ -11,5 +11,6 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        Pwoer.init().excecute()
     }
 }
