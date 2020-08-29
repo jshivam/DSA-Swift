e5a6f924c349fb104247562472aebe1850db954a
Shivam Jaiswal <>
2020-08-29 16:19:36 +0530

    update : problem statement of Convert Sorted Array to Binary Search Tree

--- DSA-Swift/SocurceCode/Tree/Convert Sorted Array to Binary Search Tree.swift
+++ DSA-Swift/SocurceCode/Tree/Convert Sorted Array to Binary Search Tree.swift
@@ -8,6 +8,26 @@
 
 import UIKit
 
+/**
+ 
+ Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+ For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+ Example:
+
+ Given the sorted array: [-10,-3,0,5,9],
+
+ One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+       0
+      / \
+    -3   9
+    /   /
+  -10  5
+
+*/
+
 extension Tree {
     static func sortedArrayToBST(_ nums: [Int]) -> TreeNode? {
         if nums.isEmpty { return nil }
