f24d3e3b9fe7b62dd5e3b1545e25cb3655e50daa
Shivam Jaiswal <>
2020-08-29 23:39:27 +0530

    update: problem statement of Coin Change

--- DSA-Swift/SocurceCode/DP/Coin Change.swift
+++ DSA-Swift/SocurceCode/DP/Coin Change.swift
@@ -7,7 +7,24 @@
 //
 
 import UIKit
+/**
+ You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
 
+ Example 1:
+
+     Input: coins = [1, 2, 5], amount = 11
+     Output: 3
+     
+     Explanation: 11 = 5 + 5 + 1
+ 
+ Example 2:
+
+     Input: coins = [2], amount = 3
+     Output: -1
+     
+ Note:
+ You may assume that you have an infinite number of each kind of coin.
+*/
 class Coin_Change: Executable {
     func excecute() {
         print(coinChange([1, 2, 5], 11))
